2cf825548a2ff84de23cca09ed188b31387431c4
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-08-06 20:53:47 -0230

    init and first page

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={require('./assets/images/bugattiChiron.jpg')}
+          style={styles.carImage}
+        />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Bugatti Chiron</Text>
+          <Text style={styles.subtitle}>$10,000/day</Text>
+        </View>
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -16,5 +27,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+
+  titles: {
+    marginTop: '20%',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: '500',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+
+  carImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    position: 'absolute'
   },
 });
