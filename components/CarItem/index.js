481ab41656139fd6bed6143c550ae03df2147a97
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-08-07 18:49:30 -0230

    formatting

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import StyledButton from '../StyledButton';
 import styles from './styles';
 
-
 const CarItem = (props) => {
-    return (
-        <View style={styles.carContainer}>
-        <ImageBackground
-          source={require('../../assets/images/bugattiChiron.jpg')}
-          style={styles.carImage}
-        />
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require('../../assets/images/bugattiChiron.jpg')}
+        style={styles.carImage}
+      />
 
-        <View style={styles.titles}>
-          <Text style={styles.title}>Bugatti Chiron</Text>
-          <Text style={styles.subtitle}>$10,000/day</Text>
-        </View>
-
-        <StyledButton />
-
-
+      <View style={styles.titles}>
+        <Text style={styles.title}>Bugatti Chiron</Text>
+        <Text style={styles.subtitle}>$10,000/day</Text>
       </View>
-    );
+
+      <StyledButton />
+    </View>
+  );
 };
 
 export default CarItem;
--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -3,19 +3,17 @@ import { View, Text, Pressable } from 'react-native';
 import styles from './styles';
 
 const StyledButton = (props) => {
-
-    const type = props.type;
+  const type = props.type;
 
   return (
     <View style={styles.container}>
-      <Pressable 
-      style={styles.button} 
-      onPress={() => {
-        console.warn('Hey there!');
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          console.warn('Hey there!');
         }}
       >
-        <Text
-        style={styles.text}>Book this car</Text>
+        <Text style={styles.text}>Book this car</Text>
       </Pressable>
     </View>
   );
